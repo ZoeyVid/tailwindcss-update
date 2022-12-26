8318c59f7d45cbe9cba04d7a418249bacdf938cf
Zoey <zoey@z0ey.de>
2022-12-26 18:21:50 +0100

    Update and rename run to run.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+
 apk add --no-cache git
 yarn add --no-lockfile -D tailwindcss $INPUT_PLUGINS
 yarn run tailwindcss -i $INPUT_INPUT -o $INPUT_OUTPUT
+git config --global --add safe.directory /github/workspace
 git add $INPUT_OUTPUT
