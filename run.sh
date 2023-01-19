09db51b198b3761f717105efc4f920d242d981a3
Zoey <zoey@z0ey.de>
2023-01-19 08:26:45 +0100

    force adding output file by git
    Signed-off-by: Zoey <zoey@z0ey.de>

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ apk add --no-cache git
 yarn add --no-lockfile -D tailwindcss $INPUT_PLUGINS
 yarn run tailwindcss --input $INPUT_INPUT --output $INPUT_OUTPUT $INPUT_PARAMS
 git config --global --add safe.directory /github/workspace
-git add $INPUT_OUTPUT
+git add --force $INPUT_OUTPUT
