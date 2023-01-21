e5e43c5d40f8ad8998aaa4d4b0370f3605e1c969
Zoey <zoey@z0ey.de>
2023-01-21 17:16:18 +0100

    Update run.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 apk add --no-cache git
-yarn add --no-lockfile -D tailwindcss $INPUT_PLUGINS
-yarn run tailwindcss --input $INPUT_INPUT --output $INPUT_OUTPUT $INPUT_PARAMS
+yarn add --no-lockfile -D tailwindcss "$INPUT_PLUGINS"
+yarn run tailwindcss --input "$INPUT_INPUT" --output "$INPUT_OUTPUT" "$INPUT_PARAMS"
 git config --global --add safe.directory /github/workspace
-git add --force $INPUT_OUTPUT
+git add --force "$INPUT_OUTPUT"
