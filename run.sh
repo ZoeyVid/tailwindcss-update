369d195c3d8a56e1ecf3b297dcd8d7986877096e
Zoey <zoey@z0ey.de>
2024-06-08 21:54:21 +0200

    Update run.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- run.sh
+++ run.sh
@@ -6,3 +6,4 @@ chmod +x /usr/local/bin/tailwindcss
 tailwindcss --input "$INPUT_INPUT" --output "$INPUT_OUTPUT" "$INPUT_PARAMS"
 git config --global --add safe.directory /github/workspace
 git add --force "$INPUT_OUTPUT"
+chmod -R 666 /github/workspace/.git
