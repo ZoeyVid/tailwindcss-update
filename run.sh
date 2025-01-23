4dd3ec6f04395490d0275bb2204add60cd8d1c5e
Zoey <zoey@z0ey.de>
2025-01-23 08:54:20 +0100

    Update run.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-apk add --no-cache git
 wget https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64-musl  -O /usr/local/bin/tailwindcss
 chmod +x /usr/local/bin/tailwindcss
 tailwindcss --input "$INPUT_INPUT" --output "$INPUT_OUTPUT" "$INPUT_PARAMS"
